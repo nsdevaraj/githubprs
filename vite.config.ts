829946ae86c9e899c388ad89a7030ff147e44b2d
Devaraj <nsdevaraj@gmail.com>
2024-11-17 22:57:13 +0530

    config keys

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = {
+    KV_URL:
+      "rediss://:AVJjAAIjcDE3NzBjMGI2MTdkNjc0OWQ2OTliZWY3M2VkNjZlNjNkN3AxMA@prime-egret-21091.upstash.io:6379",
+    KV_REST_API_READ_ONLY_TOKEN:
+      "AlJjAAIgcDHSuNys7jU_iVZh8iDjkZ7VYQKr03vUAege3MRLM0e8jw",
+    KV_REST_API_TOKEN:
+      "AVJjAAIjcDE3NzBjMGI2MTdkNjc0OWQ2OTliZWY3M2VkNjZlNjNkN3AxMA",
+    KV_REST_API_URL: "https://prime-egret-21091.upstash.io",
+  };
 
   return {
     plugins: [react()],
@@ -18,5 +26,5 @@ export default defineConfig(({ command, mode }) => {
         return prev;
       }, {}),
     },
-  }
-})
+  };
+});
